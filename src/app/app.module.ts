d964b89ef3d068421efc9f7789cae11fae1b4a75
suganya0708 <jsuganya0708@gmail.com>
2020-10-07 20:35:46 +0530

    login page

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
